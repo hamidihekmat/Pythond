42aeb02ab57addda449d1e10d62e8f72acb2d4b1
Hekmat Hamidi <theonehamidi@gmail.com>
2019-09-05 19:31:05 -0400

    added connector

--- OOP-LogicGates/logicgates.py
+++ OOP-LogicGates/logicgates.py
@@ -1,9 +1,144 @@
+class LogicGate:
+
+    def __init__(self,n):
+        self.name = n
+        self.output = None
+
+    def getLabel(self):
+        return self.name
+
+    def getOutput(self):
+        self.output = self.performGateLogic()
+        return self.output
+
+
+class BinaryGate(LogicGate):
+
+    def __init__(self,n):
+        super(BinaryGate, self).__init__(n)
+
+        self.pinA = None
+        self.pinB = None
+
+    def getPinA(self):
+        if self.pinA == None:
+            return int(input("Enter Pin A input for gate "+self.getLabel()+"-->"))
+        else:
+            return self.pinA.getFrom().getOutput()
+
+    def getPinB(self):
+        if self.pinB == None:
+            return int(input("Enter Pin B input for gate "+self.getLabel()+"-->"))
+        else:
+            return self.pinB.getFrom().getOutput()
+
+    def setNextPin(self,source):
+        if self.pinA == None:
+            self.pinA = source
+        else:
+            if self.pinB == None:
+                self.pinB = source
+            else:
+                print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class AndGate(BinaryGate):
+
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a==1 and b==1:
+            return 1
+        else:
+            return 0
+
+class OrGate(BinaryGate):
+
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a ==1 or b==1:
+            return 1
+        else:
+            return 0
+
+class UnaryGate(LogicGate):
+
+    def __init__(self,n):
+        LogicGate.__init__(self,n)
+
+        self.pin = None
+
+    def getPin(self):
+        if self.pin == None:
+            return int(input("Enter Pin input for gate "+self.getLabel()+"-->"))
+        else:
+            return self.pin.getFrom().getOutput()
+
+    def setNextPin(self,source):
+        if self.pin == None:
+            self.pin = source
+        else:
+            print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class NotGate(UnaryGate):
+
+    def __init__(self,n):
+        UnaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+        if self.getPin():
+            return 0
+        else:
+            return 1
+
+
+class Connector:
+
+    def __init__(self, fgate, tgate):
+        self.fromgate = fgate
+        self.togate = tgate
+
+        tgate.setNextPin(self)
+
+    def getFrom(self):
+        return self.fromgate
+
+    def getTo(self):
+        return self.togate
+
+
+def main():
+   g1 = AndGate("G1")
+   g2 = AndGate("G2")
+   g3 = OrGate("G3")
+   g4 = NotGate("G4")
+   c1 = Connector(g1,g3)
+   c2 = Connector(g2,g3)
+   c3 = Connector(g3,g4)
+   print(g4.getOutput())
+
+main()
+
+
+
+
+
 
 '''
 @auth: Hekmat Hamidi
 @purpose: To learn class inheritance
 '''
-
+'''
 class LogicGate:
 
     def __init__(self, label):
@@ -17,13 +152,6 @@ class LogicGate:
         self.output = self.performGateLogic() # -> this method will be performed on the child class
         return self.output
 
-    '''
-    Creating a subclass of LogicGate based on number of pins
-    Binary -> 2
-    Unary -> 2
-    Binary -> AND OR
-    Unary -> NOT
-    '''
 
 class BinaryGate(LogicGate):
 
@@ -34,12 +162,24 @@ class BinaryGate(LogicGate):
         self.pinB = None
 
     def getPinA(self):
-        return int(input('Enter Pin A input for the gate {} --> '.format(self.label)))
-
+        if self.pinA == None:
+            return int(input('Enter Pin A input for the gate {} --> '.format(self.label)))
+        else:
+            return self.pinA.getFrom().getOutput()
 
     def getPinB(self):
-        return int(input('Enter Pin B input for the gate {} --> '.format(self.label)))
-
+        if self.pinB == None:
+            return int(input('Enter Pin B input for the gate {} --> '.format(self.label)))
+        else:
+            return self.pinB.getFrom().getOutput()
+    def setNextPin(self, source):
+        if self.pinA == None:
+            self.pinA = source
+        else:
+            if self.pinB == None:
+                self.pinB = source
+            else:
+                raise RunTimeError('ErrorL No EMPTY PINS!')
 
 class UnaryGate(LogicGate):
 
@@ -52,9 +192,6 @@ class UnaryGate(LogicGate):
         return int(input('Enter Pin input for the gate {} --> '.format(self.label)))
 
 
-'''
-super(self).__init__(param)
-'''
 
 
 class AndGate(BinaryGate):
@@ -62,7 +199,7 @@ class AndGate(BinaryGate):
     def __init__(self, label):
         super(AndGate, self).__init__(label)
 
-    def performGateLogic(self): # hidden function used by the parent class 1st in the hierchy, we can use getOutput function to call this method 
+    def performGateLogic(self): # hidden function used by the parent class 1st in the hierchy, we can use getOutput function to call this method
 
         a = self.getPinA()
         b = self.getPinB()
@@ -72,6 +209,21 @@ class AndGate(BinaryGate):
             return 0
 
 
-g1 = AndGate('g1')
-out = g1.getOutput()
-print(out)
+
+class Connector:
+
+    def __init__(self, fgate, tgate):
+        self.fromgate = fgate
+        self.togate = tgate
+
+        tgate.setNextPin(self)
+
+    def getFrom(self):
+        return self.fromgate
+
+    def getTo(self):
+        return self.togate
+
+
+
+'''
